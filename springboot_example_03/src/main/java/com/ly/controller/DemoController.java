1b9dd87673a175365b523a3c226e2326a5dd597a
8989-9898 <https://github.com/8989-9898/Utils.git>
2019-05-05 11:17:55 +0800

    springboot回顾第七章，整合com.battcn.swagger-spring-boot-starter.1.4.5-RELEASE版本的swagger  api接口文档

--- springboot_example_03/src/main/java/com/ly/controller/DemoController.java
+++ springboot_example_03/src/main/java/com/ly/controller/DemoController.java
@@ -3,8 +3,6 @@ package com.ly.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
